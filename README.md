bef4ed9e0c3609d1c1afd2891fea345d321b81eb
Denis Carriere <carriere.denis@gmail.com>
2022-08-23 22:53:18 -0400

    add example

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# GET_BLOCK_NUM
-GET_BLOCK_NUM Example
+# EOSIO `GET_BLOCK_NUM` Example
+
+## CDT
+
+- [blanc++ v0.12.0](https://github.com/haderech/blanc)
+
+## Quickstart
+
+```bash
+$ blanc++ exxample.cpp
+$ cleos set contract <ACCOUNT> . example.wasm example.abi
+```
---
+++ example.cpp
@@ -0,0 +1,25 @@
+#include <eosio/eosio.hpp>
+#include <eosio/system.hpp>
+
+using namespace eosio;
+
+class [[eosio::contract]] example : public contract {
+    public:
+    using contract::contract;
+
+    [[eosio::action]]
+    void getblock()
+    {
+        const uint32_t block_num = eosio::internal_use_do_not_use::get_block_num();
+        getblocklog_action getblocklog( get_self(), { get_self(), "active"_n });
+        getblocklog.send(block_num);
+    }
+
+    [[eosio::action]]
+    void getblocklog( const uint32_t block_num ) {
+        print(block_num);
+    }
+
+    using getblock_action = action_wrapper<"getblock"_n, &example::getblock>;
+    using getblocklog_action = action_wrapper<"getblocklog"_n, &example::getblocklog>;
+};
